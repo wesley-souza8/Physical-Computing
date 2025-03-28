e3930ce2b5b9fda66d9bb98e09b411dcf976950e
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-28 20:09:38 -0300

    Criação do projeto da aula do dia 28/03/2025

---
+++ 2025_03_28/README.md
@@ -0,0 +1,101 @@
+## 1. Instalação das Bibliotecas Necessárias
+
+### **Se estiver usando os computadores da FIAP:**
+
+1. Abra o **Anaconda Navigator**.
+![ANACONDA NAVIGATOR](/2025_03_07/assets/img/AnacondaNavigator.png)
+2. No **CMD.exe Prompt**, execute os seguintes comandos:
+
+### **Se estiver no seu próprio computador:**
+
+No terminal, execute:
+
+```sh
+pip install opencv-python
+pip install matplotlib
+pip install notebook
+pip install pyserial  # Integração do Arduino com Python
+pip install mediapipe
+pip install --upgrade mediapipe opencv-python numpy
+pip install matplotlib opencv-python notebook pyserial mediapipe
+pip install matplotlib notebook opencv-python pyserial mediapipe numpy
+```
+
+## 2. Componentes Usados no Circuito
+
+Os seguintes componentes são utilizados no projeto:
+
+- **Arduino Uno**
+- **Board**
+- **Led Amarelo**
+- **Led Verde**
+- **Led Vermelho**
+- **3 Resistores**
+
+O circuito montado no SimulIDE ficará assim:
+
+![Circuito](/2025_03_21/assets/img/circuito.png)
+
+### **Configuração da Porta Serial no Código**
+No arquivo **face.py**, edite a linha **8** para corresponder porta informada pelo arduino IDE (exemplo: **COM5**):
+
+```python
+arduino = serial.Serial('COM5', 9600, timeout=1)
+```
+
+## 3. Usando Webcam ou MP4
+
+O código principal é **face.py**.
+
+Existem duas formas de rodar o código, dependendo da entrada de vídeo desejada.
+
+### **1. Usar a Webcam**
+Se deseja utilizar a webcam para detecção, altere a linha **18** do código **face.py**, substituindo `video_path` por `0`:
+
+```python
+cap = cv2.VideoCapture(0)
+```
+
+### **2. Usar um Vídeo MP4**
+Caso prefira utilizar um vídeo pré-gravado, mantenha a variável `video_path`
+
+```python
+cap = cv2.VideoCapture(video_path)
+```
+
+## 4. Executando o Código
+Aula de hoje foi para mostrar uma funcionalidade do MediaPipe, com o reconhecimento dos olhos.
+O mediaPipe identifica os valores do olho aberto e do olho fechado.
+Quando o olho está aberto, o led verde fica ligado.
+Quando o olho está piscando, o led amarelo fica ligado e caso o olho fique muito tempo fechado, o led vermelho acenderá.
+
+```python
+if ear <= LIMIAR_FECHAR:
+    frames_fechado += 1
+
+    if frames_fechado == 3:
+        piscadas += 1
+        arduino.write(b'Y')  # Piscada = LED verde (normal)
+
+    elif frames_fechado > LIMIAR_COCHILO:
+        arduino.write(b'R')  # Cochilo detectado = LED vermelho + buzzer
+
+elif ear > LIMIAR_ABRIR:
+    frames_fechado = 0
+    arduino.write(b'G')  # Olhos abertos = estado normal
+```
+
+1. Execute o código arduino.py no terminal:
+    ```sh
+    python face.py
+    ```
+
+2. Vídeo de demonstração
+![Demonstração](/2025_03_21/assets/vdi/demonstracao.mp4)
+
+## 5. Observações Finais
+
+* Caso o vídeo não abra, verifique se o arquivo selfie.mp4 está localizado corretamente em assets/vid/.
+
+
+
---
+++ 2025_03_28/video_pose.py
@@ -0,0 +1,75 @@
+import cv2                          # Biblioteca para vídeo e imagens
+import mediapipe as mp              # Biblioteca do Google para reconhecimento corporal
+import numpy as np
+import os                  # Para cálculos matemáticos (vetores e ângulos)
+
+# Inicializa o modelo de detecção de pose
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
+mp_draw = mp.solutions.drawing_utils  # Para desenhar na tela
+
+# Contador de repetições
+contador = 0
+fase = None  # 'descendo' ou 'subindo'
+
+video_path = os.path.join(os.getcwd(), '2025_03_28', 'assets', 'vid', 'exercicio.mp4')
+
+# Abre a webcam
+cap = cv2.VideoCapture(video_path)
+
+while True:
+    ret, frame = cap.read()          # Captura o vídeo frame a frame
+    if not ret:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)  # Volta ao início do vídeo
+        continue
+        #break
+    frame = cv2.resize(frame, (500, 500))
+    frame = cv2.flip(frame, 1)       # Espelha o vídeo (efeito espelho)
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Converte para RGB (MediaPipe espera RGB)
+    resultado = pose.process(rgb)    # Processa a pose no frame atual
+
+    if resultado.pose_landmarks:     # Se algum corpo for detectado
+        mp_draw.draw_landmarks(frame, resultado.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+        # Pegando os pontos do braço direito
+        pontos = resultado.pose_landmarks.landmark
+        ombro = [pontos[12].x * frame.shape[1], pontos[12].y * frame.shape[0]]     # Ponto do ombro direito
+        cotovelo = [pontos[14].x * frame.shape[1], pontos[14].y * frame.shape[0]]  # Cotovelo direito
+        punho = [pontos[16].x * frame.shape[1], pontos[16].y * frame.shape[0]]     # Pulso direito
+
+        # Cálculo direto do ângulo entre ombro, cotovelo e punho
+        a = np.array(ombro)
+        b = np.array(cotovelo)
+        c = np.array(punho)
+
+        angulo = np.degrees(np.arctan2(c[1]-b[1], c[0]-b[0]) -
+                            np.arctan2(a[1]-b[1], a[0]-b[0]))
+        angulo = np.abs(angulo)
+        if angulo > 180:
+            angulo = 360 - angulo
+
+        # Exibe o ângulo na tela
+        cv2.putText(frame, f"Angulo: {int(angulo)}", (10, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
+
+        # Lógica da repetição
+        if angulo > 160:
+            fase = 'descendo'
+        if angulo < 40 and fase == 'descendo':
+            fase = 'subindo'
+            contador += 1
+
+        # Exibe o número de repetições
+        cv2.putText(frame, f"Repeticoes: {contador}", (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    # Mostra o vídeo com as informações
+    cv2.imshow("Rosca Biceps - Pose Detection", frame)
+
+    # Encerra se apertar 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Finaliza a aplicação
+cap.release()
+cv2.destroyAllWindows()
